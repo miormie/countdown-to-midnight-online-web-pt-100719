1f0dc4686314cc17a3eb5f74cdb005efa8c183a9
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-21 02:10:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
-#write your code here
+#write your code here 
+# method #method with integer arg. use while loop to countdown to 0 
+#puts out "#{number} SECOND(S)!" in each iteration of the loop.
 
-def countdown
+def countdown(number)
+   number = 11
+   while number > 1 
+      number -= 1 
+   puts "#{number} SECOND(S)!" 
+end 
+  return "HAPPY NEW YEAR!" 
 end
+
+
+
+
+def countdown_with_sleep(number)
+  number = 11
+  while number > 1 
+  puts "#{number} SECOND(S)!"
+    sleep(1.0)
+end
+return "HAPPY NEW YEAR!"
+end 
